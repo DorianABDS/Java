8e88919aec789543677139347a4dcbb83e7f392a
DorianABDS <abbadessadorian@gmail.com>
2025-07-17 22:13:35 +0200

    feat: add lombok

---
+++ src/main/java/exercice/poo/Main.java
@@ -0,0 +1,57 @@
+package exercice.poo;
+
+import exercice.poo.model.*;
+import exercice.poo.multilevel.*;
+
+public class Main {
+    // Point d'entrée du programme
+    public static void main(String[] args) {
+
+        Car myCar = new Car("BMW", "Red", 500);
+        // Affichage des informations de la voiture
+        System.out.println("Information de la voiture:");
+        System.out.println("Marque: " + myCar.getBrand());
+        System.out.println("Couleur: " + myCar.getColor());
+        System.out.println("Vitesse: " + myCar.getSpeed() + "km/h");
+        System.out.println("-------------------------------------");
+
+        Car myCar1 = new Car("Audi", "Jaune", 1000);
+        // Affichage des informations de la voiture
+        System.out.println("Information de la voiture:");
+        System.out.println("Marque: " + myCar1.getBrand());
+        System.out.println("Couleur: " + myCar1.getColor());
+        System.out.println("Vitesse: " + myCar1.getSpeed() + "km/h");
+        System.out.println("-------------------------------------");
+
+        Phone myPhone = new Phone("Pixel", "Android", "Blanc");
+        // Affichage des informations du téléphone
+        System.out.println("Information du téléphone:");
+        System.out.println("Marque: " + myPhone.getBrand());
+        System.out.println("Système d'exploitation: " + myPhone.getOs());
+        System.out.println("Couleur: " + myPhone.getColor());
+        System.out.println("-------------------------------------");
+
+        Mammal myMammal = new Mammal(
+                "Moumout",       // name
+                120.0,           // height
+                80.5,            // weight
+                "Herbivore",     // diet
+                "Forêt",         // habitat
+                180,             // gestationDuration
+                4,               // numberOfLegs
+                38.5,            // bodyTemperature
+                true             // hasFur
+        );
+
+        System.out.println("Nom: " + myMammal.getName());
+        System.out.println("Taille: " + myMammal.getHeight());
+        System.out.println("Poids: " + myMammal.getWeight());
+        System.out.println("Régime: " + myMammal.getDiet());
+        System.out.println("Habitat: " + myMammal.getHabitat());
+        System.out.println("Gestation: " + myMammal.getGestationDuration() + " jours");
+        System.out.println("Nombre de pattes: " + myMammal.getNumberOfLegs());
+        System.out.println("Température corporelle: " + myMammal.getBodyTemperature() + "°C");
+        System.out.println("A des poils ? " + (myMammal.isHasFur() ? "Oui" : "Non"));
+
+    }
+}
